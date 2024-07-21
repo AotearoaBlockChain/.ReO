13402dc4e383ed6faa9508dffc0a937859beb717
Nirmata <iroam.tbs@proton.me>
2024-07-21 13:42:26 +1200

    Update manage.rs
    Signed-off-by: Nirmata <iroam.tbs@proton.me>

--- src/manage.rs
+++ src/manage.rs
@@ -1,36 +1,36 @@
 // src/manage.rs
 
-pub struct HangangaKōnae {
-    // Whakatakotoria ngā āpure i konei
+pub struct HangangaKonae {
+    // Whakatakotoria nga apure i konei
     pub ingoa: String,
     pub rahi: u64,
 }
 
-impl HangangaKōnae {
+impl HangangaKonae {
     pub fn hou(ingoa: String, rahi: u64) -> Self {
-        HangangaKōnae { ingoa, rahi }
+        HangangaKonae { ingoa, rahi }
     }
 
-    pub fn tāpirihia_kōnae(&self, kōnae: &str) {
-        // Rautaki tāpiri kōnae i konei
-        println!("Kōnae '{}' kua tāpirihia ki '{}'", kōnae, self. ingoa);
+    pub fn tapirihia_konae(&self, konae: &str) {
+        // Rautaki tapiri konae i konei
+        println!("Konae '{}' kua tapirihia ki '{}'", konae, self. ingoa);
     }
 
-    pub fn muku_kōnae(&self, kōnae: &str) {
-        // Rautaki muku kōnae i konei
-        println!("Kōnae '{}' kua mukua i '{}'", kōnae, self. ingoa);
+    pub fn muku_konae(&self, konae: &str) {
+        // Rautaki muku konae i konei
+        println!("Konae '{}' kua mukua i '{}'", konae, self. ingoa);
     }
 
-    pub fn rārangi_kōnae(&self) {
-        // Rautaki rārangi kōnae i konei
-        println!("Rārangi kōnae kei roto i '{}'", self. ingoa);
+    pub fn rarangi_konae(&self) {
+        // Rautaki rarangi konae i konei
+        println!("Rarangi konae kei roto i '{}'", self. ingoa);
     }
 }
 
-pub fn whakahaere_kōnae() {
+pub fn whakahaere_konae() {
     // Tinana o te mahi i konei
-    let kōnae = HangangaKōnae::hou("Tauira Kōnae".to_string(), 1024);
-    kōnae.tāpirihia_kōnae("tauira.txt");
-    kōnae.muku_kōnae("tauira.txt");
-    kōnae.rārangi_kōnae();
+    let konae = HangangaKonae::hou("Tauira Konae".to_string(), 1024);
+    konae.tapirihia_konae("tauira.txt");
+    konae.muku_konae("tauira.txt");
+    konae.rarangi_konae();
 }
