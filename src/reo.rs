71882760211cc0220ad2262f46ef40cf3fec0e40
Nirmata <iroam.tbs@proton.me>
2024-08-01 20:25:37 +1200

    Update reo.rs
    Signed-off-by: Nirmata <iroam.tbs@proton.me>

--- src/reo.rs
+++ src/reo.rs
@@ -1,5 +1,3 @@
-mod crypto;
-
 use ring::digest::{Context, Digest, SHA256};
 use ring::hmac;
 
@@ -28,7 +26,7 @@ impl ReoScript {
             code: code.to_string(),
         }
     }
-    
+
     pub fn execute(&self) {
         if self.code.contains("tātari_raraunga") {
             tātari_raraunga();
@@ -48,41 +46,6 @@ impl ReoScript {
 }
 
 fn tātari_raraunga() {
-    // Tauira mahi tātari raraunga
-    let raraunga = vec![1, 2, 3, 4, 5];
-    let tapeke: i32 = raraunga.iter().sum();
-    println!("Kua tatauria ngā raraunga: {}", tapeke);
-
-    let toharite = tapeke as f32 / raraunga.len() as f32;
-    println!("Ko te toharite: {}", toharite);
-
-    let tino_teitei = raraunga.iter().max().unwrap();
-    println!("Ko te tino teitei: {}", tino_teitei);
-}
-
-    pub fn execute(&self) {
-        if self.code.contains("analyze_data") {
-            analyze_data();
-        } else {
-            println!("Kāore he mahi mō tēnei kōwae.");
-        }
-    }
-}
-
-fn analyze_data() {
-    // Example data analysis function
-    let data = vec![1, 2, 3, 4, 5];
-    let sum: i32 = data.iter().sum();
-    println!("Kua tatauria ngā raraunga: {}", sum);
-
-    let mean = sum as f32 / data.len() as f32;
-    println!("Ko te toharite: {}", mean);
-
-    let max = data.iter().max().unwrap();
-    println!("Ko te tino teitei: {}", max);
-}
-
-fn tatari_raraunga() {
     // Tauira mahi tātari raraunga
     let raraunga = vec![1, 2, 3, 4, 5];
     let tapeke: i32 = raraunga.iter().sum();
