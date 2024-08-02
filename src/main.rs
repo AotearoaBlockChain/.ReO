a719156a9e4dafdb76a81c779e6dcd594083b4be
Nirmata <iroam.tbs@proton.me>
2024-08-03 00:47:53 +1200

    Update main.rs
    Signed-off-by: Nirmata <iroam.tbs@proton.me>

--- src/main.rs
+++ src/main.rs
@@ -8,7 +8,7 @@ mod crypto;
 use crate::consensus::{Purotu, RopuRaraunga, whakamana, tumomo_hoko};
 use crate::interpretation::{TauiraHanganga, Whakamaamatanga, whakamaramatia_korero};
 use crate::manage::HangangaKonae;
-use crate::crypto::{hangaia_kīwaha_matua, waitohua_raraunga, whakaū_waitohu};
+use crate::crypto::{hangaia_kiwaha_matua, waitohua_raraunga, whakau_waitohu};
 use crate::reo::{ReoScript, whakamuna_raraunga, hangaia_hmac, tapirihia_konae, mukua_konae, rarangi_konae};
 
 fn main() {
@@ -21,21 +21,21 @@ fn main() {
     ];
 
     for waehere in whakahau {
-        let hōtaka = ReoScript::hou(waehere);
-        hōtaka.whakahaere();
+        let hotaka = ReoScript::hou(waehere);
+        hotaka.whakahaere();
     }
 
     let waehere = "whakamuna_raraunga";
-    let hōtaka = ReoScript::hou(waehere);
-    hōtaka.whakahaere();
+    let hotaka = ReoScript::hou(waehere);
+    hotaka.whakahaere();
 
     let waehere = "hangaia_hmac";
-    let hōtaka = ReoScript::hou(waehere);
-    hōtaka.whakahaere();
+    let hotaka = ReoScript::hou(waehere);
+    hotaka.whakahaere();
 
     let waehere = "tātari_raraunga";
-    let hōtaka = ReoScript::hou(waehere);
-    hōtaka.whakahaere();
+    let hotaka = ReoScript::hou(waehere);
+    hotaka.whakahaere();
 
     let mut ropu_raraunga = RopuRaraunga {
         // Whakatūngia ngā āpure i konei mēnā e tika ana
@@ -66,12 +66,12 @@ fn main() {
     konae.rarangi_konae();
 
     // Tauira ECDSA
-    match hangaia_kīwaha_matua() {
-        Ok((kī_muna, kī_tūmatanui)) => {
+    match hangaia_kiwaha_matua() {
+        Ok((ki_muna, ki_tumatanui)) => {
             let raraunga = b"tauira raraunga".to_vec();
-            match waitohua_raraunga(&kī_muna, &raraunga) {
+            match waitohua_raraunga(&ki_muna, &raraunga) {
                 Ok(waitohu) => {
-                    match whakaū_waitohu(&kī_tūmatanui, &raraunga, &waitohu) {
+                    match whakau_waitohu(&ki_tumatanui, &raraunga, &waitohu) {
                         Ok(he_tika) => println!("He tika te waitohu: {}", he_tika),
                         Err(e) => println!("Hapa i te whakaū waitohu: {}", e),
                     }
@@ -79,6 +79,6 @@ fn main() {
                 Err(e) => println!("Hapa i te waitohua raraunga: {}", e),
             }
         },
-        Err(e) => println!("Hapa i te waihanga kīwaha matua: {}", e),
+        Err(e) => println!("Hapa i te waihanga kiwaha matua: {}", e),
     }
 }
