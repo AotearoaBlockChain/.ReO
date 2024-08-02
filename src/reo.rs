995382723b51e5d16a98f26138b60c06cdf49654
Nirmata <iroam.tbs@proton.me>
2024-08-03 00:43:55 +1200

    Update reo.rs
    Signed-off-by: Nirmata <iroam.tbs@proton.me>

--- src/reo.rs
+++ src/reo.rs
@@ -14,34 +14,34 @@ pub fn whakamuna_raraunga(raraunga: &str) -> Result<String, Box<dyn Error>> {
 }
 
 // Waihangahia te HMAC (Create HMAC)
-pub fn hangaia_hmac(kī: &str, raraunga: &str) -> Result<String, Box<dyn Error>> {
-    let hmac_kī = hmac::Key::new(hmac::HMAC_SHA256, kī.as_bytes());
-    let waitohu = hmac::sign(&hmac_kī, raraunga.as_bytes());
+pub fn hangaia_hmac(ki: &str, raraunga: &str) -> Result<String, Box<dyn Error>> {
+    let hmac_ki = hmac::Key::new(hmac::HMAC_SHA256, ki.as_bytes());
+    let waitohu = hmac::sign(&hmac_ki, raraunga.as_bytes());
     Ok(hex::encode(waitohu.as_ref()))
 }
 
 // Waihangahia te kīwaha matua (Create keypair)
-pub fn hangaia_kīwaha_matua() -> Result<(Vec<u8>, Vec<u8>), Box<dyn Error>> {
+pub fn hangaia_kiwaha_matua() -> Result<(Vec<u8>, Vec<u8>), Box<dyn Error>> {
     let rng = SystemRandom::new();
     let pkcs8_bytes = EcdsaKeyPair::generate_pkcs8(&ECDSA_P256_SHA256_FIXED_SIGNING, &rng)?;
-    let kīwaha_matua = EcdsaKeyPair::from_pkcs8(&pkcs8_bytes)?;
+    let kiwaha_matua = EcdsaKeyPair::from_pkcs8(&pkcs8_bytes)?;
 
-    let kī_muna = pkcs8_bytes.as_ref().to_vec();
-    let kī_tūmatanui = kīwaha_matua.public_key().as_ref().to_vec();
+    let ki_muna = pkcs8_bytes.as_ref().to_vec();
+    let ku_tumatanui = kiwaha_matua.public_key().as_ref().to_vec();
 
-    Ok((kī_muna, kī_tūmatanui))
+    Ok((ki_muna, ki_tumatanui))
 }
 
 // Waitohua ngā raraunga (Sign data)
-pub fn waitohua_raraunga(kī_muna: &[u8], raraunga: &[u8]) -> Result<Vec<u8>, Box<dyn Error>> {
-    let kīwaha_matua = EcdsaKeyPair::from_pkcs8(kī_muna)?;
+pub fn waitohua_raraunga(ki_muna: &[u8], raraunga: &[u8]) -> Result<Vec<u8>, Box<dyn Error>> {
+    let kiwaha_matua = EcdsaKeyPair::from_pkcs8(ki_muna)?;
     let rng = SystemRandom::new();
-    let waitohu = kīwaha_matua.sign(&rng, raraunga)?;
+    let waitohu = kiwaha_matua.sign(&rng, raraunga)?;
     Ok(waitohu.as_ref().to_vec())
 }
 
 // Whakaūngia te waitohu (Verify signature)
-pub fn whakaū_waitohu(kī_tūmatanui: &[u8], raraunga: &[u8], waitohu: &[u8]) -> Result<bool, Box<dyn Error>> {
-    let kī_tūmatanui = UnparsedPublicKey::new(&ECDSA_P256_SHA256_FIXED, kī_tūmatanui);
-    kī_tūmatanui.verify(raraunga, waitohu).map_err(|_| "Waitohu kāore i te tika".into())
+pub fn whakau_waitohu(ki_tumatanui: &[u8], raraunga: &[u8], waitohu: &[u8]) -> Result<bool, Box<dyn Error>> {
+    let ki_tumatanui = UnparsedPublicKey::new(&ECDSA_P256_SHA256_FIXED, ki_tumatanui);
+    ki_tumatanui.verify(raraunga, waitohu).map_err(|_| "Waitohu kāore i te tika".into())
 }
