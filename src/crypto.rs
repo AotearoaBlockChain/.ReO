299ebb6525d954a6dbbfea7ab7e32ee8f9cfcc6e
Nirmata <iroam.tbs@proton.me>
2024-08-06 11:10:43 +1200

    Update crypto.rs
    Signed-off-by: Nirmata <iroam.tbs@proton.me>

--- src/crypto.rs
+++ src/crypto.rs
@@ -1,8 +1,9 @@
+extern crate ring;
 use ring::digest::{Context, SHA256};
 use ring::hmac;
 use ring::signature::{EcdsaKeyPair, KeyPair, ECDSA_P256_SHA256_FIXED_SIGNING, ECDSA_P256_SHA256_FIXED};
 use ring::rand::SystemRandom;
-use ring::signature::{UnparsedPublicKey};
+use ring::signature::UnparsedPublicKey;
 use std::error::Error;
 
 // Whakamunatia nga raraunga (Hash data)
@@ -24,10 +25,10 @@ pub fn hangaia_hmac(ki: &str, raraunga: &str) -> Result<String, Box<dyn Error>> 
 pub fn hangaia_kiwaha_matua() -> Result<(Vec<u8>, Vec<u8>), Box<dyn Error>> {
     let rng = SystemRandom::new();
     let pkcs8_bytes = EcdsaKeyPair::generate_pkcs8(&ECDSA_P256_SHA256_FIXED_SIGNING, &rng)
-        .map_err(|_| "Failed to generate key pair")?;
+        .map_err(|_| "I rahua te waihanga i te kiwaha matua")?;
     let pkcs8_bytes_ref = pkcs8_bytes.as_ref();
     let kiwaha_matua = EcdsaKeyPair::from_pkcs8(&ECDSA_P256_SHA256_FIXED_SIGNING, pkcs8_bytes_ref)
-        .map_err(|_| "Failed to create key pair from pkcs8")?;
+        .map_err(|_| "I rahua te waihanga i te kiwaha matua mai i te pkcs8")?;
 
     let ki_muna = pkcs8_bytes_ref.to_vec();
     let ki_tumatanui = kiwaha_matua.public_key().as_ref().to_vec();
@@ -38,10 +39,10 @@ pub fn hangaia_kiwaha_matua() -> Result<(Vec<u8>, Vec<u8>), Box<dyn Error>> {
 // Waitohua nga raraunga (Sign data)
 pub fn waitohua_raraunga(ki_muna: &[u8], raraunga: &[u8]) -> Result<Vec<u8>, Box<dyn Error>> {
     let kiwaha_matua = EcdsaKeyPair::from_pkcs8(&ECDSA_P256_SHA256_FIXED_SIGNING, ki_muna)
-        .map_err(|_| "Failed to create key pair from pkcs8")?;
+        .map_err(|_| "I rahua te waihanga i te kiwaha matua mai i te pkcs8")?;
     let rng = SystemRandom::new();
     let waitohu = kiwaha_matua.sign(&rng, raraunga)
-        .map_err(|_| "Failed to sign data")?;
+        .map_err(|_| "I rahua te waitohu i nga raraunga")?;
     Ok(waitohu.as_ref().to_vec())
 }
 
@@ -49,4 +50,4 @@ pub fn waitohua_raraunga(ki_muna: &[u8], raraunga: &[u8]) -> Result<Vec<u8>, Box
 pub fn whakau_waitohu(ki_tumatanui: &[u8], raraunga: &[u8], waitohu: &[u8]) -> Result<bool, Box<dyn Error>> {
     let ki_tumatanui = UnparsedPublicKey::new(&ECDSA_P256_SHA256_FIXED, ki_tumatanui);
     ki_tumatanui.verify(raraunga, waitohu).map(|_| true).map_err(|_| "Waitohu kaore i te tika".into())
-    }
+}
